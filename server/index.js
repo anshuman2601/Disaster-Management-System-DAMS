9ffdc96c7b9eb03ccde8c44e18bf2fdb4ed7c186
mac-matt <mattandcoinc@gmail.com>
2022-10-03 19:35:14 -0500

    example request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+
+app.get('/login', (req, res) => {
+    console.log(req);
+    res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send('Hello World!')
+})
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+
