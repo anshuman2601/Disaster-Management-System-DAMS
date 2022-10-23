75806e2bde9c00aa8bc4c49a9029c060306a34f5
Anshuman Sahu <anshuman2601@gmail.com>
2022-10-22 19:46:57 -0500

    Updated user primary Key

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     const Users = sequelize.define("users", {
-      username: {type: DataTypes.STRING, allowNull: false},
+      username: {type: DataTypes.STRING, allowNull: false, primaryKey: true},
         password: {type: DataTypes.STRING, allowNull: false},
         email: {type: DataTypes.STRING, allowNull: false},
         first_name: {type: DataTypes.STRING, allowNull: false},
